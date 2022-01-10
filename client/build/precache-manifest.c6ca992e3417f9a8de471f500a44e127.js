83dabc97122d14d729e893070ce99ee0e6b823e0
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-09 21:36:06 -0700

    npm run build

---
+++ client/build/precache-manifest.c6ca992e3417f9a8de471f500a44e127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d38b55214e25f7d44e97e32a14d5b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea6a45bc3c99b8adcca",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "f19efba21e73526aff88",
+    "url": "/static/js/2.8b630681.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.8b630681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea6a45bc3c99b8adcca",
+    "url": "/static/js/main.44080d64.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
